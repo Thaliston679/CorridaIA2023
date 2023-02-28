a20ec372ef6e22ca9154e851d27bec832e9fcc29
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2023-02-28 09:11:07 -0300

    Waypoints
    Performance aleatória dos carros;

--- CorridaIA2023/Assets/Scripts/Carro.cs
+++ CorridaIA2023/Assets/Scripts/Carro.cs
@@ -7,19 +7,44 @@ public class Carro : MonoBehaviour
 {
     NavMeshAgent car;
 
-    public GameObject destiny;
+    public GameObject[] wayPoints;
+    public int destiny = 0;
+
+    private float randomPerformance = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         car = GetComponent<NavMeshAgent>();
 
-        car.SetDestination(destiny.transform.position);
+        car.SetDestination(wayPoints[destiny].transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Waypoints
+        if(Vector3.Distance(transform.position, wayPoints[destiny].transform.position) < 3)
+        {
+            destiny++;
+            if (destiny > wayPoints.Length - 1) destiny = 0;
+
+            car.SetDestination(wayPoints[destiny].transform.position);
+        }
+
+        //Velocidade e aceleração aleatória
+        if (randomPerformance >= 0) randomPerformance -= Time.deltaTime;
+        if (randomPerformance <= 0)
+        {
+            RandomizeCarPerformance();
+        }
+        //--
+    }
+
+    void RandomizeCarPerformance()
+    {
+        randomPerformance = 5;
+        car.acceleration = Random.Range(4, 8);
+        car.speed = Random.Range(10, 17);
     }
 }
